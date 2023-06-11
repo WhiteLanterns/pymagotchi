e1d6972da39f9c3c1f5ec91a244f9225d9e02496
WhiteLanterns <willschfr@gmail.com>
2023-06-10 23:49:46 -0400

    Initial Commit

---
+++ classes.py
@@ -0,0 +1,9 @@
+import pyxel
+
+class Pet:
+    def __init__(self, health, fun, hunger, energy, mood):
+        self.health = health
+        self.fun = fun
+        self.hunger = hunger
+        self.energy = energy
+        self.mood = mood
